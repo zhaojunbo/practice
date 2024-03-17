6160f84286c1fe16bcf1527e9838fb26d3eb545f
zhaojunbo <zhjb1994@163.com>
2024-03-17 13:49:43 +0800

    compsoer update

--- src/MyPlugin.php
+++ src/MyPlugin.php
@@ -3,8 +3,7 @@ declare(strict_types=1);
 require __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
-use Sywlwl\ComposerSet\Command\GetTimeCommand;
-
+use Zhaojunbo\ComposerSet\Command\GetTimeCommand;
 
 $application = new Application();
 $application->add(new GetTimeCommand());
